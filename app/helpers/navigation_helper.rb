441ad769cef3337f8e29cbee534e198213b56a7c
Robert Buchberger <rbuchberger@gmail.com>
2018-03-14 15:49:32 +0100

    extracted partials, added logic to helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,2 +1,9 @@
 module NavigationHelper
+  def collapsible_elements
+    if user_signed_in? 
+      'layouts/navigation/signed_in_links'
+    else
+      'layouts/navigation/signed_out_links'
+    end
+  end
 end
