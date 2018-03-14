bbfd2a6e56b36474f06b5f8486fbf36314671953
Robert Buchberger <rbuchberger@gmail.com>
2018-03-14 19:02:51 +0100

    removed unnecessary test folder, configured rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,14 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/poltergeist'
+require 'factory_bot_rails'
+require 'capybara/rspec'
+
+config.include Devise::Test::IntegrationHelpers, type: :feature
+config.include FactoryBot::Syntax::Methods
+Capybara.javascript_driver = :poltergeist
+Capybara.server = :puma
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -33,7 +41,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +62,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  # FCC config code
+
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation); }
+
+  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+
+  config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation; }
+
+  config.before(:each) { DatabaseCleaner.start }
+
+  config.after(:each) { DatabaseCleaner.clean }
+
 end
